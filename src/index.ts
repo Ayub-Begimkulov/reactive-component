b5098e898a301d8ee7757ad66167478acd9c8975
Ayub-Begimkulov <begimkulov963@gmail.com>
2020-04-27 10:37:41 +0300

    add example

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,27 @@ import { observable, observe } from "./observable";
 import { component } from "./component";
 
 interface IState {
-  text: string;
-  count: number;
   user: {
     name: string;
     age: number;
   };
+  counter: {
+    value: number;
+  };
 }
+
+const useCounter = (el: Element) => {
+  // would be better to make ref
+  const counter = observable({
+    value: 0
+  });
+
+  observe(() => {
+    el.textContent = "" + counter.value;
+  });
+
+  return counter;
+};
 
 const app = component<IState>({
   root: "#app",
@@ -18,44 +32,37 @@ const app = component<IState>({
     button: "#btn",
     count: "#count"
   },
+  // aka vue composition api)
   setup({ elements }) {
-    const state = observable({
-      text: "hello world",
-      count: 0,
-      user: {
-        name: "hello",
-        age: 16
-      }
+    const user = observable({
+      name: "hello",
+      age: 16
     });
 
-    const { text, count } = elements;
+    const { count } = elements;
 
-    observe(() => {
-      text.textContent = state.text;
-    });
-
-    observe(() => {
-      count.textContent = `${state.count}`;
-    });
+    const counter = useCounter(count);
 
     observe(() => {
       // @TODO make a reactions resetting for nested objects
-      console.log(state.user.name);
+      console.log(user.name);
     });
 
-    return state;
+    return { user, counter };
   },
   beforeDestroy({ elements, state }) {
     console.log(state, elements);
   },
   events: {
+    // also it's may be better to declare event handlers
+    // in setup function and use event object as a map
+    // with same key but with value as method name
     "input on input"(e, state) {
       const value = (e.target as HTMLInputElement).value;
-      state.text = value;
       state.user.name = value;
     },
     "click on button"(_e, state) {
-      state.count++;
+      state.counter.value++;
     }
   }
 });
